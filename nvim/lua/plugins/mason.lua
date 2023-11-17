2e250d2cf471dcc55b3dc54831059ba45a31863d
Kacper Krylowicz <kacper.krylowicz@greenteg.com>
2023-11-17 10:31:52 +0100

    add pyright to lspconfig.lua for python autocompletion

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,30 +1,20 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
+    "williamboman/mason.nvim",
+    dependencies = {"williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim"},
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-        ensure_installed = {
-            "pyright",
-            "ruff",
-            "mypy",
-        }
-    })
+        mason.setup()
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "rust_analyzer",
-        "sqlls",
-      },
-      automatic_installation = true
-    })
-  end,
+        mason_lspconfig.setup({
+            ensure_installed = {"html", "cssls", "tailwindcss", "lua_ls", "rust_analyzer", "sqlls", "pyright"},
+            automatic_installation = true
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {"pyright", "ruff", "mypy"}
+        })
+    end
 }
