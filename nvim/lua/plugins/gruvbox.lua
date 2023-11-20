d92facd3d2c0567dc21f1c2bb4964d815c57597e
Kacper Krylowicz <kacper.krylowicz@greenteg.com>
2023-11-20 19:04:48 +0100

    more gruvbox changes

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,10 +1,11 @@
 return {
     "eddyekofo94/gruvbox-flat.nvim",
     config = function()
-        vim.o.background = "dark"
         vim.g.gruvbox_flat_style = "dark"
         vim.g.gruvbox_italic_keywords = false
         vim.cmd[[colorscheme gruvbox-flat]]
+        vim.api.nvim_set_hl(0, "@include.python", { link = "Boolean" })
+        vim.api.nvim_set_hl(0, "@attribute.python", { link = "TSParameter" })
     end
 }
 
